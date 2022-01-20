a3945dde693e14ae5330bee672a89057d4067701
ProIvanko <ivanko1432@gmail.com>
2022-01-20 15:40:36 +0300

    Code optimization, Loader added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import react from "react";
+import react, { useEffect } from "react";
 import TodoList from "./Todo/TDlist";
 import context from "./context";
 import AddTodo from "./Todo/AddTodo";
+import Loader from "./Loader";
 
 
 
 function App() {
-  const [todos, setTodos] = react.useState([
-    {id:1, completed: false, title: 'Buy bread'},
-    {id:2, completed: true, title: 'Buy butter'},
-    {id:3, completed: false, title: 'Buy milk'}
-  ])
+  const [todos, setTodos] = react.useState([])
+
+  const [Loading, setLoading] = react.useState(true)
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then(response => response.json())
+      .then(todos => {
+        setTimeout(() =>{
+          setTodos(todos)
+          setLoading(false)
+        }, 2000)
+      })
+  }, [] )
 
   function toggleTodo(id) {
     setTodos (
@@ -44,15 +54,18 @@ function App() {
     <div className="wrapper">
       <h1>React tutorial</h1>
       
-      <AddTodo onCreate={addTodo}></AddTodo>
+      <AddTodo onCreate={addTodo}/>
+
+      {Loading && <Loader/>}
+
       {todos.length ? (
-      
       <TodoList 
       todos={todos} 
       onToggle={toggleTodo}
       />
-      ) : (
-        <p>No todos!</p>
+
+      ) : Loading ? null : (
+          <p>No todos!</p>
       )}
       
     </div>
